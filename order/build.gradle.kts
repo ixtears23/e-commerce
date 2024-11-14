95055a0a38b87664be97e63353278286518a1a9a
JunseokOh <ixtears23@icloud.com>
2024-11-14 18:55:38 +0900

    Add useJUnitPlatform to gradle

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -6,3 +6,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
