f42f49eaab4bd14c183255743e447352f5409c3e
JunseokOh <ixtears23@icloud.com>
2024-11-14 23:26:43 +0900

    Add r2dbc connection pool

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -1,4 +1,5 @@
 val jasyncMysqlVersion = "2.2.0"
+val r2dbcPoolVersion = "1.0.2.RELEASE"
 
 dependencies {
     implementation(project(":core"))
@@ -6,6 +7,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.github.jasync-sql:jasync-mysql:$jasyncMysqlVersion")
+    implementation("io.r2dbc:r2dbc-pool:${r2dbcPoolVersion}")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
